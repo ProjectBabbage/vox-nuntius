0cd0ffbd5f093e31f0ecbeb4c3f5b37589da8cc7
mlgarchery <martial.garchery@gmail.com>
2023-05-27 17:22:46 +0200

    add Unit

--- godot-project/scripts/Unit.gd
+++ godot-project/scripts/Unit.gd
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_RigidBody2D_body_entered(body:Node):
+	print("collide with ", body)
