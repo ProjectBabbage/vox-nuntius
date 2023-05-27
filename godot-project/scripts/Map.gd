1dc48a2bdd46f2b303eb09e2c65695ecd512e765
abaudhuin <adrien.baudhuin@altirnao.com>
2023-05-27 16:09:44 +0200

    path from A to B

--- godot-project/scripts/Map.gd
+++ godot-project/scripts/Map.gd
@@ -4,6 +4,9 @@ extends Area2D
 # var a = 2
 # var b = "text"
 
+var isWritingPath := false
+var currentLine: Line2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,19 +15,25 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if isWritingPath:
+		currentLine.points[1] = get_global_mouse_position()
 
 
 func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		print("click")
-		var position = get_global_mouse_position()
-		var l := Line2D.new()
-		l.antialiased = true
-		l.default_color = Color(0, 0, 0, 1)
-		l.width = 3
-		l.add_point(Vector2(0, 0))
-		l.add_point(position)
-		add_child(l)
+		if isWritingPath:
+			isWritingPath = false
+			currentLine.points[1] = get_global_mouse_position()
+			currentLine = null
+		else:
+			isWritingPath = true
+			currentLine = Line2D.new()
+			currentLine.antialiased = true
+			currentLine.default_color = Color(0, 0, 0, 1)
+			currentLine.width = 3
+			currentLine.add_point(get_global_mouse_position())
+			currentLine.add_point(get_global_mouse_position())
+			add_child(currentLine)
 	pass  # Replace with function body.
