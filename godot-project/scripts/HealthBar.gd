54ea5e8df1c8dd977fd1c59d6033d6693bdd1227
abaudhuin <adrien.baudhuin@altirnao.com>
2023-05-28 21:13:39 +0200

    fix imports

--- godot-project/scripts/HealthBar.gd
+++ godot-project/scripts/HealthBar.gd
@@ -4,11 +4,11 @@ extends ProgressBar
 func _ready():
 	assert(get_parent() is Building or get_parent() is Unit, "Parent has no health")
 	var team = get_parent().team
-	if team == 'player':
-		self.get('custom_styles/fg/bg_color').set_bg_color(Color(110, 0, 110, 110))
+	if team == "player":
+		self.get("custom_styles/fg/bg_color").set_bg_color(Color(110, 0, 110, 110))
 	else:
-		self.get('custom_styles/fg/bg_color').set_bg_color(Color(0, 110, 110, 110))
+		self.get("custom_styles/fg/bg_color").set_bg_color(Color(0, 110, 110, 110))
+
 
 func _process(_delta):
 	value = get_parent().health.current_health
-
