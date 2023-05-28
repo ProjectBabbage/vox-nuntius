904448c56f4499e3856537b5652a3b7c919ee80e
Quentin Garchery <garchery.quentin@gmail.com>
2023-05-28 12:51:28 +0200

    sensible default parameters to test

--- godot-project/scripts/Messenger.gd
+++ godot-project/scripts/Messenger.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export var movement_speed: float = 100
+export var movement_speed: float = 200
 export var messageScene: PackedScene
 
 var order: Order
@@ -10,7 +10,7 @@ func _physics_process(delta):
 	if order:
 		if order.origin.distance_to(position) < 1:
 			var message = messageScene.instance()
-			message.position = position
+			message.position = order.origin
 			message.order = order
 			get_tree().get_root().add_child(message)
 			queue_free()
