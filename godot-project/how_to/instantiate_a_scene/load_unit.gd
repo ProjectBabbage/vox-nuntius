5a0625dc9df0245e50668a24018d71278a093879
mlgarchery <martial.garchery@gmail.com>
2023-05-29 02:30:38 +0200

    new obstacle

--- godot-project/how_to/instantiate_a_scene/load_unit.gd
+++ godot-project/how_to/instantiate_a_scene/load_unit.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var resource = preload("res://scenes/Unit.tscn")
+var soldierScene = preload("res://scenes/Soldier.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,10 +8,10 @@ var resource = preload("res://scenes/Unit.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var unit = resource.instance()
-	unit.translate(Vector2.RIGHT * 200 + Vector2.DOWN * 100)
-	add_child(unit)
-	add_child(resource.instance())
+	var soldier = soldierScene.instance()
+	soldier.translate(Vector2.RIGHT * 200 + Vector2.DOWN * 100)
+	add_child(soldier)
+	add_child(soldierScene.instance())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
