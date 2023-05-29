8b84b667d2a3b2b75e5b65dfb8df4af5ab76a0dd
mlgarchery <martial.garchery@gmail.com>
2023-05-29 10:15:13 +0200

    add  hover for order path

--- godot-project/scripts/Message.gd
+++ godot-project/scripts/Message.gd
@@ -2,3 +2,35 @@ extends Area2D
 class_name Message
 
 var order: Order
+var line: Line2D
+
+func set_order(_order: Order):
+	order = _order
+	# create Line2D
+	# var new_line = Line2D.new()
+	# new_line.add_point(Vector2.ZERO)
+	# new_line.add_point(Vector2.RIGHT * 500)
+	# new_line.antialiased = true
+	# new_line.default_color = Color(200, 0, 0, 1)
+	# new_line.width = 5
+	# add_child(new_line)
+	# test add to Map
+
+
+
+	line = Line2D.new()
+	line.add_point(Vector2.ZERO)
+	line.add_point(_order.points[1] - _order.points[0])
+	line.antialiased = true
+	line.default_color = Color(0, 0, 0, 1)
+	line.width = 3
+	add_child(line)
+	line.visible = false
+
+func _on_Message_mouse_exited():
+	if line:
+		line.visible = false
+
+func _on_Message_mouse_entered():
+	if line:
+		line.visible = true
--- godot-project/scripts/Messenger.gd
+++ godot-project/scripts/Messenger.gd
@@ -11,7 +11,7 @@ func _physics_process(delta):
 		if order.points[0].distance_to(position) < 10:
 			var message = messageScene.instance()
 			message.position = order.points[0]
-			message.order = order
+			message.set_order(order)
 			get_tree().get_root().add_child(message)
 			var player_id = get_tree().get_network_unique_id()
 			game.players[player_id]["available_nuntius"] +=1
