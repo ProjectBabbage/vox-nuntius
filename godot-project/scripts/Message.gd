6f55291ff39da7830e790d09bd472912a7e3ff40
mlgarchery <martial.garchery@gmail.com>
2023-05-29 10:18:59 +0200

    add messenger path multipoints

--- godot-project/scripts/Message.gd
+++ godot-project/scripts/Message.gd
@@ -6,21 +6,11 @@ var line: Line2D
 
 func set_order(_order: Order):
 	order = _order
-	# create Line2D
-	# var new_line = Line2D.new()
-	# new_line.add_point(Vector2.ZERO)
-	# new_line.add_point(Vector2.RIGHT * 500)
-	# new_line.antialiased = true
-	# new_line.default_color = Color(200, 0, 0, 1)
-	# new_line.width = 5
-	# add_child(new_line)
-	# test add to Map
-
-
-
+	# show line of order
 	line = Line2D.new()
 	line.add_point(Vector2.ZERO)
-	line.add_point(_order.points[1] - _order.points[0])
+	for i in range(1, order.points.size()):
+		line.add_point(_order.points[i] - _order.points[0])
 	line.antialiased = true
 	line.default_color = Color(0, 0, 0, 1)
 	line.width = 3
